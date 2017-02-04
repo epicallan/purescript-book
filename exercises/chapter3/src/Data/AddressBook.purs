52ca9cb0c62be3f5862d75c4a6f45d0c3f932d10
allan <epicallan.al@gmail.com>
2017-02-04 20:25:05 +0300

    exercise 3 attempt

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -1,9 +1,9 @@
 module Data.AddressBook where
 
 import Prelude
-
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.BooleanAlgebra (class BooleanAlgebra)
+import Data.List (List(..), filter, head, null, nubBy)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -37,3 +37,24 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet streetName = head <<< filter filterForStreet
+  where
+  filterForStreet :: Entry -> Boolean
+  filterForStreet entry = entry.address.street == streetName
+
+print :: Maybe Entry -> Maybe String
+print entry = map showEntry entry
+
+printByStreet :: String -> AddressBook -> Maybe String
+printByStreet streetName book = print $ findEntryByStreet streetName book
+
+isNameInAddressBook :: String -> AddressBook -> Boolean
+isNameInAddressBook firstName = null <<< filter filterByFirstName
+  where
+  filterByFirstName :: Entry -> Boolean
+  filterByFirstName entry = entry.firstName == firstName
+
+-- removeDuplicates :: AddressBook -> AddressBook
+-- removeDuplicates =  <<< nubBy(\x y => x.firstName == y.firstName)
